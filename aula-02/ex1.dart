be9344342cc0fadeb3579f0b8652fffbaab95d8e
Thales Godoi <thalesgodoi1@gmail.com>
2025-02-03 19:37:22 -0300

    Exercicios

---
+++ aula-02/ex1.dart
@@ -0,0 +1,9 @@
+// importando a biblioteca do Dart
+import "dart:io";
+// função principal do codigo
+void main(){
+  //declarando a variavel do tipo inteira
+  //chamada idade
+  int idade = 29;
+  print("Idade: $idade");
+}
