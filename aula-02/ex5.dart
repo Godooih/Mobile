b1e99ee5d8c7576e7bdf2fe08a6c7f57653ec512
Thales Godoi <thalesgodoi1@gmail.com>
2025-02-03 22:16:38 -0300

    Exercicios

--- aula-02/ex5.dart
+++ aula-02/ex5.dart
@@ -5,10 +5,10 @@ import 'dart:io';
 void main(){
   print("Digite n1:");
   double num1 = double.parse(stdin.readLineSync()!);
-  
+
   print("Digite n2:");
   double num2 = double.parse(stdin.readLineSync()!);
   
   double res = num1 * num2;
-  print("Res= $res");
+  print("Res= $res"); 
 }
