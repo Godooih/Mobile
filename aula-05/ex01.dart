88758e98aff1d8e2bb2ffcc9495298db8d46b53a
Thales Godoi <thalesgodoi1@gmail.com>
2025-02-24 19:30:00 -0300

    Exercicios

--- aula-05/ex01.dart
+++ aula-05/ex01.dart
@@ -2,7 +2,7 @@
 
 //Criar classe chamada Casa
 class Casa{
-  String? cor; //
+  String? cor; // ? faz parte do null safety do dart que é para garantir que a variavel possa inicializar sem valores
   int? qtde_p;//
 
   //Cria o metodo chamado abrirporta()
@@ -13,10 +13,10 @@ class Casa{
 
 void main(){
   //Instancia o objeto chamado minhaCasa
-  Casa minhaCasa = Casa();
-  minhaCasa.cor="Azul";
+  Casa minhaCasa = Casa();//cria um objeto dentro da classe "CASA"
+  minhaCasa.cor="Azul"; //Define que a cor do objeto "Minha casa" é azul
   minhaCasa.qtde_p=2;
   minhaCasa.AbrirPorta();
   print("Cor da casa: ${minhaCasa.cor}");
-  print("A casa tem ${minhaCasa.qtde_p} portas")
+  print("A casa tem ${minhaCasa.qtde_p} portas");
   }
